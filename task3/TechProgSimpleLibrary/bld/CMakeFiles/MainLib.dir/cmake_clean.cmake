0b3952dbc5ffaa0cd62822fb5e9be453c916b0b9
klimarissa17 <mariaprotas@yandex.ru>
2021-05-28 05:54:10 +0300

    add TechProgSimpleLibrary repo

---
+++ task3/TechProgSimpleLibrary/bld/CMakeFiles/MainLib.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MainLib.dir/main_lib.cpp.o"
+  "../bin/MainLib.pdb"
+  "../bin/MainLib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MainLib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
