91a07635ceb5677f1ed5aa4a3fc9c37ad57d360b
klimarissa17 <mariaprotas@yandex.ru>
2021-05-28 06:45:14 +0300

    fixes

--- task3/TechProgSimpleLibrary/Toolchain.cmake
+++ task3/TechProgSimpleLibrary/Toolchain.cmake
@@ -4,9 +4,8 @@ set(CMAKE_SYSTEM_PROCESSOR arm)
 get_filename_component(CMAKE_SYSROOT "${CMAKE_CURRENT_LIST_DIR}/../sysroot" ABSOLUTE) 
 get_filename_component(toolchain "${CMAKE_CURRENT_LIST_DIR}/../toolchain" ABSOLUTE) 
 
-#set(CMAKE_STAGING_PREFIX "${CMAKE_CURRENT_LIST_DIR}/stage")
+set(CMAKE_STAGING_PREFIX "${CMAKE_CURRENT_LIST_DIR}/stage")
 
-#set(tools /home/devel/gcc-4.7-linaro-rpi-gnueabihf)
 set(tools "${toolchain}/gcc-linaro-i686_arm-linux-gnueabihf")
 
 set(CMAKE_C_COMPILER ${tools}/bin/arm-linux-gnueabihf-gcc)
